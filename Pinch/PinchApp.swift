34f185b837c7bd52334f8c425a2ca955850a557b
Tony Gorez <gorez.tony@gmail.com>
2023-05-22 08:28:59 +0200

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Tony Gorez on 22/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
