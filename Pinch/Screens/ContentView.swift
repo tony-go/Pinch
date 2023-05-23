af96e2b30a8bdb54cb3fd5aedff1c709b7c744ba
Tony Gorez <gorez.tony@gmail.com>
2023-05-23 20:18:42 +0200

    feat: add drag gesture for main view

--- Pinch/Screens/ContentView.swift
+++ Pinch/Screens/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var isAnimating: Bool = false
-    
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
+    
+    private func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     
     var body: some View {
         NavigationView {
@@ -22,12 +29,27 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
                     .onTapGesture(count: 2, perform: {
                         withAnimation(.spring()) {
-                            imageScale = imageScale == 1 ? 5 : 1
+                            if imageScale == 1 { imageScale = 5 }
+                            else { resetImageState() }
+                                    
                         }
                     })
+                    .gesture(
+                        DragGesture()
+                            .onChanged{ value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            }.onEnded{ _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            }
+                    )
             } //: Zstack
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
