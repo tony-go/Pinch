6b0b9986ad17d326834688cef12084620a20cfc0
Tony Gorez <gorez.tony@gmail.com>
2023-05-24 17:14:28 +0200

    feat: add base info panel

---
+++ Pinch/UI/InfoPanelUI.swift
@@ -0,0 +1,54 @@
+//
+//  InfoPanelUI.swift
+//  Pinch
+//
+//  Created by Tony Gorez on 24/05/2023.
+//
+
+import SwiftUI
+
+struct InfoPanelUI: View {
+    var scale: CGFloat
+    var offset: CGSize
+    
+    var body: some View {
+        HStack {
+            // MARK - HITSPOT
+            Image(systemName: "circle.circle")
+                .symbolRenderingMode(.hierarchical)
+                .resizable()
+                .frame(width: 30, height: 30)
+            
+            Spacer()
+           
+            HStack(spacing: 2) {
+                Image(systemName: "arrow.up.left.and.arrow.down.right")
+                Text("\(scale)")
+                
+                Spacer()
+               
+                Image(systemName: "arrow.left.and.right")
+                Text("\(offset.width)")
+
+                Spacer()
+
+                Image(systemName: "arrow.left.and.right")
+                Text("\(offset.height)")
+            }
+            .font(.footnote)
+            .padding(8)
+            .background(.ultraThinMaterial)
+            .cornerRadius(8)
+            .frame(maxWidth: 420)
+        }
+    }
+}
+
+struct InfoPanelUI_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoPanelUI(scale: 1, offset: .zero)
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
